e08d67aa86d809f711217e9382c1d24e5eaf332d
Alexandra Bafile <acbafile@gmail.com>
2021-12-05 13:45:24 -0500

    some frontend stuff

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+      <h1 class="h3 mb-3 font-weight-normal" id="h1">Please Sign In</h1>
       <div
         class="alert alert-danger"
         role="alert"
@@ -12,7 +12,6 @@
         role="alert"
         v-if="this.$route.query.registration"
       >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
       <input
         type="text"
         id="username"
@@ -22,7 +21,6 @@
         required
         autofocus
       />
-      <label for="password" class="sr-only">Password</label>
       <input
         type="password"
         id="password"
@@ -31,18 +29,21 @@
         v-model="user.password"
         required
       />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
+      <router-link :to="{ name: 'register' }" id="need-account">Need an account?</router-link>
+      <button type="submit" id="sign-in-button">Sign in</button>
     </form>
+    <p id="description">Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.
+    Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
   </div>
 </template>
 
 <script>
 import authService from "../services/AuthService";
+import LoginStyle from "../styles/LoginStyle.css";
 
 export default {
   name: "login",
-  components: {},
+  components: LoginStyle,
   data() {
     return {
       user: {
@@ -74,3 +75,6 @@ export default {
   }
 };
 </script>
+
+<style>
+</style>
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="register" class="text-center">
     <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+      <h1 class="h3 mb-3 font-weight-normal" id="create">Create Account</h1>
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         {{ registrationErrorMsg }}
       </div>
-      <label for="username" class="sr-only">Username</label>
       <input
         type="text"
         id="username"
@@ -15,7 +14,6 @@
         required
         autofocus
       />
-      <label for="password" class="sr-only">Password</label>
       <input
         type="password"
         id="password"
@@ -32,25 +30,39 @@
         v-model="user.confirmPassword"
         required
       />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
+      <label for="parent" class="parent-checkbox">
+      <input type="checkbox" id="parent" class="form-control" v-model="user.isParent" />
+      Are you a parent?</label>
+      <router-link :to="{ name: 'login' }" id="have-account">Have an account?</router-link>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" id="button">
         Create Account
       </button>
     </form>
+    <div id="register-description">
+<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be?
+He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. 
+They called me Mr Glass. </p>
+<p>Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. 
+But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. 
+We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. </p>
+    </div>
   </div>
 </template>
 
 <script>
 import authService from '../services/AuthService';
+import RegisterStyles from '../styles/RegisterStyle.css';
 
 export default {
   name: 'register',
+  components: RegisterStyles,
   data() {
     return {
       user: {
         username: '',
         password: '',
         confirmPassword: '',
+        isParent: false,
         role: 'user',
       },
       registrationErrors: false,
@@ -89,5 +101,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
