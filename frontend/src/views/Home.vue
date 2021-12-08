869ae3643abff92ca676f2cb2101a90434228ce1
Alexandra Bafile <acbafile@gmail.com>
2021-12-08 16:21:53 -0500

    nav bar and such

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,12 +3,7 @@
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
     <div id="nav">
-      <router-link v-bind:to="{name: 'create-family'}" class="create-family"> Parent Page </router-link>
-      &nbsp;|&nbsp;
-      <router-link v-bind:to="{name: 'add-prize'}" class="add-prize"> Add a Prize </router-link>
     </div>
-    
-  
     <search-bar />
   </div>
 </template>
