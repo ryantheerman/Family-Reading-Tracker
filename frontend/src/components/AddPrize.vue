a95bde3db03526f7abfcf6e301d5909f793c1389
Alexandra Bafile <acbafile@gmail.com>
2021-12-08 09:10:24 -0500

    AddPrizes and styles

--- frontend/src/components/AddPrize.vue
+++ frontend/src/components/AddPrize.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
-      <form>
-          <label>Prize Name: 
+      <form class="prize-form">
+          <label class="prize-name">Prize Name: 
           <input
             type="text"
             id="prize-name"
             class="form-control"
             placeholder="Name your prize"
-            v-model="prize.name"
+            v-model="newPrize.name"
             required
             autofocus
         /> </label>
-        <label>Prize Description: 
+        <label class="prize-description">Prize Description: 
         <input
             type="text"
             id="prize-description"
             class="form-control"
-            placeholder="description of prize"
-            v-model="prize.description"
+            placeholder="Description of prize"
+            v-model="newPrize.description"
             required
         /> </label>
-        <label>Minute Milestone: 
+        <label class="milestone">Minute Milestone: 
         <input
             type="number"
             id="milestone"
@@ -28,49 +28,51 @@
             step="10"
             min="0"
             placeholder="Minutes to Read"
-            v-model="prize.milestone"
+            v-model="newPrize.milestone"
             required
         /> </label>
-        <label>Number of Winners: 
+        <label class="maxPrizes">Number of Winners: 
         <input
             type="number"
             id="maxPrizes"
             class="form-control"
-            placeholder="Number of Prizes Available"
+            placeholder="Number of winners"
             min="1"
-            v-model="prize.maxPrizes"
+            v-model="newPrize.maxPrizes"
             required
         /> </label>
-        <label>Start Date: 
+        <label class="startDate">Start Date: 
         <input
             type="date"
             id="startDate"
             class="form-control"
             placeholder=""
-            v-model="prize.startDate"
+            v-model="newPrize.startDate"
             required
         /> </label>
-        <label>End Date: 
+        <label class="endDate">End Date: 
         <input
             type="date"
             id="endDate"
             class="form-control"
             placeholder=""
-            v-model="prize.endDate"
+            v-model="newPrize.endDate"
             required
         /> </label>
-        <button>Submit Prize!</button>
+        <button id="submit-button">Submit Prize!</button>
       </form>
       
   </div>
 </template>
 
 <script>
+import AddPrizeStyle from '../styles/AddPrizeStyle.css';
 export default {
     name: "add-prize",
+    component: {AddPrizeStyle},
     data() {
         return {
-            prize: {
+            newPrize: {
                 name: '',
                 description: '',
                 milestone: '',
