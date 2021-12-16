2b0a43ee203f5882c0b1b0a3a161fee89c4f71a1
Alexandra Bafile <acbafile@gmail.com>
2021-12-16 16:56:39 -0500

    logo, header, and font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
-    <h1 class="header">ReadMe</h1>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Montserrat&family=Readex+Pro:wght@300;304&display=swap" rel="stylesheet">
+    <img src="@/images/header.png" class="header"/>
       <div id="nav" v-if="$store.state.token != ''">
         <router-link v-bind:to="{ name: 'home' }" class="home-nav">
-        <img src="@/images/logo.png" id="logo"/>
+        <img src="@/images/leggo-my-logo.png" id="logo"/>
         </router-link>
         <router-link v-bind:to="{ name: 'add-prize' }"  v-if="$store.state.token != '' && $store.state.user.isParent != false && $store.state.user.familyId != 0" class="prize">Prizes</router-link>
         <router-link v-bind:to="{ name: 'family' }" v-if="$store.state.token != '' && $store.state.user.isParent != false" class="family">Family</router-link>
